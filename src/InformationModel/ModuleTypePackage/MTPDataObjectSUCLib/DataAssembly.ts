3f88b3e521da7c3a49fbfc7b58039cd68751b228
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-05-27 14:41:01 +0200

    Start implementing DiagnosticElement Type

--- src/InformationModel/ModuleTypePackage/MTPDataObjectSUCLib/DataAssembly.ts
+++ src/InformationModel/ModuleTypePackage/MTPDataObjectSUCLib/DataAssembly.ts
@@ -1,8 +1,24 @@
 /** MTPFreeze 01.2020 */
+import DiagnosticElement = ModuleTypePackage.DiagnosticElement;
+import LockView4 = ModuleTypePackage.LockView4;
 
 namespace ModuleTypePackage {
+    /* DataAssembly */
     export interface DataAssembly {
         TagName: String;
         TagDescription: String;
+    }
+    /* DiagnosticElement */
+    export type DiagnosticElement = DataAssembly & WQC & {
+        Logic: object;
+        Out: object;
+        OutQC: object;
+    }
+    export type LockView4 = DiagnosticElement & {
+
+    }
+    /* ??? */
+    type WQC = {
+        WQC: object;
     }
 }
