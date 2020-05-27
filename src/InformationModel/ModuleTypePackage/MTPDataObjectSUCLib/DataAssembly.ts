7b3a760096902fb83149a1edc0268620c2965262
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-05-27 14:45:50 +0200

    Add LockView4-Type

--- src/InformationModel/ModuleTypePackage/MTPDataObjectSUCLib/DataAssembly.ts
+++ src/InformationModel/ModuleTypePackage/MTPDataObjectSUCLib/DataAssembly.ts
@@ -15,7 +15,26 @@ namespace ModuleTypePackage {
         OutQC: object;
     }
     export type LockView4 = DiagnosticElement & {
-
+        ln1: object;
+        ln1En: object;
+        ln1Inv: object;
+        ln1QC: object;
+        ln1Txt: object;
+        ln2: object;
+        ln2En: object;
+        ln2Inv: object;
+        ln2QC: object;
+        ln2Txt: object;
+        ln3: object;
+        ln3En: object;
+        ln3Inv: object;
+        ln3QC: object;
+        ln3Txt: object;
+        ln4: object;
+        ln4En: object;
+        ln4Inv: object;
+        ln4QC: object;
+        ln4Txt: object;
     }
     /* ??? */
     type WQC = {
