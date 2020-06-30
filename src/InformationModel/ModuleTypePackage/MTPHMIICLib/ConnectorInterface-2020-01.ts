6f56f664158737f439d5f874d88e6be8e684b16b
Julius Lorenz <julius.lorenz@tu-dresden.de>
2020-06-30 17:26:34 +0200

    MTPHMIICLib added

---
+++ src/InformationModel/ModuleTypePackage/MTPHMIICLib/ConnectorInterface-2020-01.ts
@@ -0,0 +1,6 @@
+/** MTPFreeze 01.2020 */
+/*Connector Interface*/
+type ConnectorInterface = {};
+
+export type InformationFlowConnector = ConnectorInterface & {};
+export type MassFlowConnector = ConnectorInterface & {};
