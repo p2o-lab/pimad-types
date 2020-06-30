ab4f4957d09014f4c003d0c398dfd705e8c51884
Julius Lorenz <julius.lorenz@tu-dresden.de>
2020-07-01 00:49:19 +0200

    MTPHMISUCLib added

--- src/InformationModel/ModuleTypePackage/MTPSUCLib/MTPSet-2020-01.ts
+++ src/InformationModel/ModuleTypePackage/MTPSUCLib/MTPSet-2020-01.ts
@@ -2,10 +2,14 @@
 import {DataAssembly} from '../MTPDataObjectSUCLib/DataAssembly-2020-01';
 import {ServerAssembly} from '../MTPCommunicationSUCLib/ServerAssembly-2020-01';
 import {ServiceSet} from '../MTPServiceSUCLib/ServiceSet-2020-01';
+import {TextSet} from '../MTPTextSUCLib/TextSet-2020-01';
+import {HMISet} from '../MTPHMISUCLib/HMISet-2020-01';
 
 export type ModuleTypePackage = {
     CommunicationSet: CommunicationSet;
     ServiceSet: ServiceSet;
+    TextSet: TextSet;
+    HMISet: HMISet;
 };
 
 /* MTPSet */
