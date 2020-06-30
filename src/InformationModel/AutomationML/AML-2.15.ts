66fad8bdc7ce63287d0f6c43d520af5f35ba8d85
Julius Lorenz <julius.lorenz@tu-dresden.de>
2020-06-30 16:20:20 +0200

    double exportet Member FIX

--- src/InformationModel/AutomationML/AML-2.15.ts
+++ src/InformationModel/AutomationML/AML-2.15.ts
@@ -1,22 +1,24 @@
-type BaseAML = {
-    Name: string;
-    ID: string;
-    Version: string;
-}
+namespace AML {
+    type BaseAML = {
+        Name: string;
+        ID: string;
+        Version: string;
+    }
 
-export type InstanceHierarchy = BaseAML & {
-    InternalElement: (ModuleTypePackage | Picture | Service)[];
-}
+    export type InstanceHierarchy = BaseAML & {
+        InternalElement: (ModuleTypePackage | Picture | Service)[];
+    }
 
-export type ModuleTypePackage = BaseAML & {
-    InternalElement: object;
-}
+    export type ModuleTypePackage = BaseAML & {
+        InternalElement: object;
+    }
 
-export type Picture = BaseAML & {
-    InternalElement: object;
-}
+    export type Picture = BaseAML & {
+        InternalElement: object;
+    }
 
-export type Service = BaseAML & {
-    InternalElement: object[];
-}
+    export type Service = BaseAML & {
+        InternalElement: object[];
+    }
 
+}
